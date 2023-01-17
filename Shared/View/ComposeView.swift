34418063c6b39e38a711193faf28fa3186f94263
mhkim <xowldidi@naver.com>
2023-01-17 19:01:39 +0900

    - add ComposeView

---
+++ Shared/View/ComposeView.swift
@@ -0,0 +1,53 @@
+//
+//  ComposeView.swift
+//  SwiftUIMemo
+//
+//  Created by IMStudio on 2023/01/17.
+//
+
+import SwiftUI
+
+struct ComposeView: View {
+    
+    @EnvironmentObject var store: MemoStore
+    @Environment(\.dismiss) var dismiss
+    @State private var content: String = ""
+    
+    var body: some View {
+         NavigationView{
+            VStack {
+                //Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                TextEditor(text: $content)
+                    .padding()
+            }
+            .navigationTitle("New Memo")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar{
+                ToolbarItemGroup(placement: .navigationBarLeading)
+                {
+                    Button {
+                           dismiss()
+                        } label: {
+                        Text("Cancel")
+                    }
+                }
+                ToolbarItemGroup(placement: .navigationBarTrailing)
+                {
+                    Button {
+                            store.insert(memo: content)
+                            dismiss()
+                        } label: {
+                        Text("Save")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ComposeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ComposeView()
+            .environmentObject(MemoStore())
+    }
+}
